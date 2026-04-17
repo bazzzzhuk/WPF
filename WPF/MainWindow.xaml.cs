3f31d15cf5fc7d6be9e96e844b962d7895eb0915
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-04-17 03:13:50 +0300

    Add project files.

---
+++ WPF/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	/// 
+	public partial class MainWindow : Window
+	{
+	string str = "Start";
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btnStart_Click(object sender, RoutedEventArgs e)
+		{
+			//btnStart.Content = btnStart.Content=="Start"?"STOP":"Start";
+		}
+	}
+}
